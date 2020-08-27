01d3dfcf4371e0aed3b7ec66885f8cb72b7ba661
vonsalumbides <68180136+vonsalumbides@users.noreply.github.com>
2020-08-28 07:54:42 +0800

    Update generate-tf-file.sh

--- k8/generate-tf-file.sh
+++ k8/generate-tf-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Initiate the state of the kops to s3 bucket and amd generate a kubernetes.tf file with kubernetes cluster
-#SSH public key must be specified when running with AWS (create with `kops create secret --name awsclusterkopsvon.k8s.local sshpublickey admin -i ~/.ssh/id_rsa.pub
+# Initiate the state of the kops to s3 bucket and generate a kubernetes.tf file with kubernetes cluster
+#SSH public key must be specified when running with AWS (create with `kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
 kops replace -f kops-template.yaml --state ${STATE} --name ${CLUSTER_NAME} --force
 kops update cluster --target terraform --state ${STATE} --name ${CLUSTER_NAME} --out .
